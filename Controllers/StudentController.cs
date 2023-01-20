38ae8cfc9ff7ebcc0efe89e9f3736bc9f13c66ad
Marcel <bvmaru@proton.me>
2023-01-20 21:12:59 +0100

    Add project files.

---
+++ Controllers/StudentController.cs
@@ -0,0 +1,103 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using StudentsAppMVC.Data;
+using StudentsAppMVC.Models;
+
+namespace StudentsAppMVC.Controllers
+{
+    public class StudentController : Controller
+    {
+
+        private static IList<StudentModel> students = new List<StudentModel>()
+        {
+        new StudentModel(){ StudentId = 1, Name = "Anna", LastName = "Nowak", Age = 19, Email = "test0@wp.pl", IsActive = true },
+        new StudentModel(){ StudentId = 2, Name = "Ula", LastName = "Rak", Age = 21, Email = "test1@wp.pl", IsActive = false },
+        new StudentModel(){ StudentId = 3, Name = "Ola", LastName = "Kos", Age = 29, Email = "test3@wp.pl", IsActive = false }
+        };
+
+        private MvcStudentsContext _dbContext;
+        public StudentController(MvcStudentsContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        // GET: StudentController
+        public ActionResult Index()
+        {
+            return View(_dbContext.Students);
+        }
+
+        // GET: StudentController/Details/5
+        public ActionResult Details(int id)
+        {
+            var specificStudent = _dbContext.Students.FirstOrDefault(x => x.StudentId == id);
+            return View(specificStudent);
+
+        }
+
+        // GET: StudentController/Create
+        public ActionResult Create()
+        {
+            return View(new StudentModel());
+        }
+
+        // POST: StudentController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(StudentModel studentModel)
+        {
+            _dbContext.Students.Add(studentModel);
+            _dbContext.SaveChanges();
+            return RedirectToAction(nameof(Index));
+        }
+
+
+        // GET: StudentController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: StudentController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: StudentController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            var studentToBeDeleted = _dbContext.Students.FirstOrDefault(x => x.StudentId == id);
+            return View(studentToBeDeleted);
+
+        }
+
+        // POST: StudentController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, StudentModel studentModel)
+        {
+            var studentToBeDeleted = _dbContext.Students.FirstOrDefault(x => x.StudentId == id);
+            _dbContext.Students.Remove(studentToBeDeleted);
+            _dbContext.SaveChanges();
+            return RedirectToAction(nameof(Index));
+        }
+
+        public ActionResult ChangeActiveStatus(int id)
+        {
+            var specificStudent = _dbContext.Students.FirstOrDefault(x => x.StudentId == id);
+            specificStudent.IsActive = !specificStudent.IsActive;
+            return RedirectToAction("Details", "Student", new { id = specificStudent.StudentId });
+        }
+
+    }
+}
